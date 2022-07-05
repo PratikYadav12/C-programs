240ef111c6d8ffdbeedee9f10832fcb5b6067bab
PratikYadav12 <99651534+PratikYadav12@users.noreply.github.com>
2022-07-05 18:34:40 +0530

    Create 08.For  Demonstration.c

---
+++ Conditional statements & loop.c/08.For  Demonstration.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0;
+    for(i = 1;i <= 5;i++)
+    {
+        printf("\n I Love INDIA");
+        printf("\n I Love You");
+    }
+
+    printf("\n Thanks !!!");
+    getch();
+    return 0;
+}
