4d297a7d014d03772602a2f5d370d9cc0061de87
PratikYadav12 <99651534+PratikYadav12@users.noreply.github.com>
2022-07-11 01:08:18 +0530

    Create 14.Factorial of Given number.c

---
+++ Digit program/14.Factorial of Given number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No = 0,Fact = 1;
+    printf("\n Enter a Number = ");
+    scanf("%d",&No);
+
+    while(No != 0)
+    {
+        Fact = Fact * No;
+        No--;
+    }
+
+
+    printf("\n Factorial of given Number is %d",Fact);
+
+    getch();
+    return 0;
+}
